beeab529cd8d6b12457ded5d348278306931ab2f
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-02-23 00:59:23 -0500

    LangChain read from context file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # LangChain Demo
 
-Small project using LangChain to generate an answer to a question using OpenAI
+Small project using LangChain to generate an answer to a question.
+The AI uses the "context" provided to genrate the answer. Context is provided in the form of a link.
+Uses models provided by OpenAI.
---
+++ conversation.py
@@ -0,0 +1,58 @@
+# setup environment variables
+from dotenv import load_dotenv
+import os
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+# import LangChain packages
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+
+
+
+
+llm = ChatOpenAI()
+
+# Takes question and retrieved documents and generates an answer
+prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+
+<context>
+{context}
+</context>
+
+Question: {input}""")
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+# Convert output to string format
+output_parser = StrOutputParser()
+
+# Chain everything together
+chain = prompt | llm | output_parser
+
+# Load reference data to index
+loader = WebBaseLoader("https://en.wikipedia.org/wiki/Bitcoin")
+docs = loader.load()
+
+# Embedding model
+embeddings = OpenAIEmbeddings()
+
+# Index data into a vectorstore
+text_splitter = RecursiveCharacterTextSplitter()
+documents = text_splitter.split_documents(docs)
+vector = FAISS.from_documents(documents, embeddings)
+
+# Use retriever to dynamically select relevant documents
+retriever = vector.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+question="What is Bitcoin mining?"
+response = retrieval_chain.invoke({"input": question})
+print(question)
+print(response["answer"])
+
